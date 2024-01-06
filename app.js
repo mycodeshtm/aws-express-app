72dca7a9c070c5a00489c109a366757103cd97a9
JosephHo7 <118572699+JosephHo7@users.noreply.github.com>
2024-01-05 22:44:34 -0500

    2.4 first commit

--- app.js
+++ app.js
@@ -20,16 +20,21 @@ listObjectsCmd = new ListObjectsV2Command(listObjectsParams);
 
 s3Client.send(listObjectsCmd);
 
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+   
+});
+
 //root path to show message when the application is working
 app.get('/', (req,res) => {
-    req.status(200);
+    res.status(200);
     res.send('Hello World')
 });
 
 //return images that are located in S3 bucket
 app.get('/images', (req,res) => {
     listObjectsParams ={
-        Bucket: my-cool-local-bucket
+        Bucket: 'my-cool-local-bucket'
     }
     s3Client.send(new ListObjectsV2Command(listObjectsParams))
         .then((listObjectsResponse) => {
@@ -40,9 +45,14 @@ app.get('/images', (req,res) => {
 //uploads a file and returns a response
 app.post('/images', (req,res) => {
     const file = req.files.image
-    const fileName = req.files.imgage.name
+    const fileName = req.files.image.name
     const tempPath = `${UPLOAD_TEMP_PATH}/${fileName}`
-    file.mv(tempPath, (err) => {res.status(500)})
+    file.mv(tempPath, (err) => { if (err) {
+        console.error('Error uploading file:', err);
+        res.status(500).send('Internal Server Error');
+    } else {
+        res.status(200).send('File uploaded successfully');
+    }})
 })
 
 app.listen(PORT, (error) => {
