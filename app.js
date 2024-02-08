a103d39cc41cc5fd7098c68c99161565a36a1861
dilu <dilu.kefale@gmail.com>
2024-02-08 12:29:02 +0300

    Revert "first commit"
    This reverts commit 160d3c812f75198fb3c379eab95c8860a2095893.

--- app.js
+++ app.js
@@ -1,65 +1,63 @@
 const express = require('express');
-const fileUpload = require('express-fileupload');
-const { S3Client, PutObjectCommand, ListObjectsV2Command } = require('@aws-sdk/client-s3');
 
 const app = express();
 const PORT = 3000;
-const UPLOAD_TEMP_PATH = 'temp'; // Replace with the actual path
+const { S3Client, ListObjectsV2Command, PutObjectCommand } = require('@aws-sdk/client-s3');
+const fs = require('fs');
+const fileUpload = require('express-fileupload');
+
 const s3Client = new S3Client({
     region: 'us-east-1',
     endpoint: 'http://localhost:4566',
     forcePathStyle: true
 });
-const BUCKET_NAME = 'my-cool-local-bucket'; // Replace with your bucket name
 
-app.use(fileUpload());
+const listObjectsParams = {
+    Bucket: 'my-cool-local-bucket'
+};
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World');
+listObjectsCmd = new ListObjectsV2Command(listObjectsParams);
+
+s3Client.send(listObjectsCmd);
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+   
 });
 
-app.get('/images', async (req, res) => {
-    try {
-        const listObjectsParams = {
-            Bucket: BUCKET_NAME
-        };
-        const listObjectsResponse = await s3Client.send(new ListObjectsV2Command(listObjectsParams));
-        res.send(listObjectsResponse);
-    } catch (error) {
-        console.error('Error listing objects:', error);
+//root path to show message when the application is working
+app.get('/', (req,res) => {
+    res.status(200);
+    res.send('Hello World')
+});
+
+//return images that are located in S3 bucket
+app.get('/images', (req,res) => {
+    listObjectsParams ={
+        Bucket: 'my-cool-local-bucket'
+    }
+    s3Client.send(new ListObjectsV2Command(listObjectsParams))
+        .then((listObjectsResponse) => {
+            res.send(listObjectsResponse)
+        })
+});
+
+//uploads a file and returns a response
+app.post('/images', (req,res) => {
+    const file = req.files.image
+    const fileName = req.files.image.name
+    const tempPath = `${UPLOAD_TEMP_PATH}/${fileName}`
+    file.mv(tempPath, (err) => { if (err) {
+        console.error('Error uploading file:', err);
         res.status(500).send('Internal Server Error');
-    }
-});
-
-app.post('/images', async (req, res) => {
-    const file = req.files ? req.files.image : null;
-    if (!file) {
-        res.status(400).send('No file provided');
-        return;
-    }
-
-    const fileName = file.name;
-
-    const uploadParams = {
-        Bucket: BUCKET_NAME,
-        Key: fileName,
-        Body: file.data
-    };
-
-    try {
-        const command = new PutObjectCommand(uploadParams);
-        await s3Client.send(command);
-        console.log('File uploaded successfully');
+    } else {
         res.status(200).send('File uploaded successfully');
-    } catch (error) {
-        console.error('Error uploading file:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
+    }})
+})
 
 app.listen(PORT, (error) => {
-    if (!error)
+    if(!error)
         console.log('The app is a success!');
-    else
+    else 
         console.log('Something is terribly wrong!', error);
 });
